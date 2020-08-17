945e3b4b2d160179887565b003b8ec7ec62457a5
Romain Strock <romain.strock@gmail.com>
2020-08-17 14:54:45 +0200

    Remove strides

--- rna_learn/training_main.py
+++ rna_learn/training_main.py
@@ -97,7 +97,7 @@ def main():
         decoder_n_hidden = 100
         growth_rate = 15
         kernel_sizes = [3] + [5] * 9
-        strides = [3] + [1] * 9
+        strides = None
         dilation_rates = None
         n_layers = len(kernel_sizes)
         l2_reg = 1e-5
