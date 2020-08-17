c4588603651877909d5f48302a914e014be49912
Romain Strock <romain.strock@gmail.com>
2020-08-17 16:21:30 +0200

    Sync log and launch tensorboard

--- sync_remote_logs.sh
+++ sync_remote_logs.sh
@@ -4,4 +4,8 @@ PEMKEY=$1
 IP=$2
 RUNID=$3
 
+echo "Syncing remote logs from run ${RUNID} from ${IP}"
 rsync -avzhe "ssh -i ${PEMKEY}" ubuntu@$IP:/home/ubuntu/rna_learn/summary_log/$RUNID summary_log/
+
+echo "Launching tensorboard for run ${RUNID}"
+tensorboard --logdir=summary_log/$RUNID
